97574d0b1ecfe2770930d01bc8ccdd3d5f685a21
Matthew Dargan <matthewdargan57@gmail.com>
2024-05-09 14:25:00 -0500

    refactor: remove clippy::all and clippy::nursery warnings

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+#![warn(clippy::pedantic)]
 use chrono::{NaiveDateTime, Utc};
 use reqwest::Url;
 use scraper::{selectable::Selectable, Html, Selector};
